196d1637b740002b03c7250bfc523d1eba30cdd3
Daniel Brice <danielbrice@gmail.com>
2018-08-08 01:12:58 -0700

    brightness/audio keys working

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -2,7 +2,7 @@ import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Util.EZConfig
 
-main = xmonad =<< xmobar (additionalKeys cnfg addk)
+main = xmonad =<< xmobar (additionalKeysP (additionalKeys cnfg addk) addp)
 
 modm = mod4Mask
 
@@ -13,4 +13,12 @@ cnfg = defaultConfig
 
 addk =
     [ ((modm, xK_p), spawn "j4-dmenu-desktop")
+    ]
+
+addp =
+    [ ("<XF86MonBrightnessUp>"  , spawn "backlight raise 500"        )
+    , ("<XF86MonBrightnessDown>", spawn "backlight lower 500"        )
+    , ("<XF86AudioMute>"        , spawn "amixer -q set Master toggle"   )
+    , ("<XF86AudioLowerVolume>" , spawn "amixer -q set Master 5- unmute")
+    , ("<XF86AudioRaiseVolume>" , spawn "amixer -q set Master 5+ unmute")
     ]
